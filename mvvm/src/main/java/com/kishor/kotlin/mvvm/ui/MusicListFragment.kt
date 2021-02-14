939e2d4db52667d6055c7ba8b87c4613816bc0c7
Kishor Sutar <kishor007sutar@gmail.com>
2021-02-14 10:07:47 +0000

    MusicListViewModel added

--- mvvm/src/main/java/com/kishor/kotlin/mvvm/ui/MusicListFragment.kt
+++ mvvm/src/main/java/com/kishor/kotlin/mvvm/ui/MusicListFragment.kt
@@ -1,8 +1,25 @@
 package com.kishor.kotlin.mvvm.ui
 
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.kishor.kotlin.mvvm.R
+import com.kishor.kotlin.mvvm.viewmodel.MusicListViewModel
 
 class MusicListFragment : Fragment(R.layout.fragment_music_list) {
+
+    private val viewModel: MusicListViewModel by viewModels()
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        Log.i("MusicListFragment","ViewModel created")
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
 
 }
