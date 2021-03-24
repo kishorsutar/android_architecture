9a9a441c2a67010ffb06c75686c002a1e515a3b7
Kishor Sutar <kishor.sutar@tigerspike.com>
2021-03-24 10:59:11 +0000

    view model added

--- mvvm/src/androidTest/java/com/kishor/kotlin/mvvm/ui/MainActivityTests.kt
+++ mvvm/src/androidTest/java/com/kishor/kotlin/mvvm/ui/MainActivityTests.kt
@@ -1,2 +1,24 @@
 package com.kishor.kotlin.mvvm.ui
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.kishor.kotlin.mvvm.R
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTests {
+    @get:Rule
+    var activityRule: ActivityScenarioRule<MainActivity>
+            = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun testEvent() {
+        onView(withId(R.id.counter)).check(matches(withText("0")))
+    }
+}
--- mvvm/src/main/java/com/kishor/kotlin/mvvm/ui/MainActivity.kt
+++ mvvm/src/main/java/com/kishor/kotlin/mvvm/ui/MainActivity.kt
@@ -5,12 +5,15 @@ import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
 import com.kishor.kotlin.mvvm.R
 import com.kishor.kotlin.mvvm.viewmodel.MainViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+    private lateinit var viewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        counter.text = viewModel.displayCounter()
     }
 }
