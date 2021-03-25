4622e9adc1907514b513e64d4deb63e5190ffc54
Kishor Sutar <kishor.sutar@tigerspike.com>
2021-03-25 23:28:47 +0000

    Added mvp architecture structure

---
+++ mvp/src/main/java/com/kishor/architecture/mvp/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.kishor.architecture.mvp.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.kishor.architecture.mvp.R
+
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
