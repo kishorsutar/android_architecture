843f319c02812b0bc5821127fb493779dd66b607
Kishor Sutar <kishor007sutar@gmail.com>
2021-03-27 21:55:34 +0000

    Kotlin by lazy block

--- kotlin_ideomatic/src/main/java/com/kishor/kotlin/ideomatic/ByLazy.kt
+++ kotlin_ideomatic/src/main/java/com/kishor/kotlin/ideomatic/ByLazy.kt
@@ -1,6 +1,7 @@
 package com.kishor.kotlin.ideomatic
 
 import java.lang.Thread.sleep
+import kotlin.random.Random
 
 fun main() {
 // Memory will be created and stored the evaluation in a memory for heavy running tasks
@@ -13,4 +14,20 @@ fun main() {
     println(name)
     println(name)
     println(name)
+
+
+    // lazy block
+
+    val result = lazy { evaluateLazyBlock() }
+
+    println(result.value)
+    println(result.value)
+    println(result.value)
+
+}
+
+fun evaluateLazyBlock(): Int {
+    println("computed")
+    sleep(1000)
+    return Random(System.currentTimeMillis()).nextInt()
 }
