54c2e94a956dab18510dd81e1075e528a7e058c6
Kishor Sutar <kishor007sutar@gmail.com>
2021-03-31 11:33:47 +0100

    Builder pattern started

---
+++ designpatterns/src/main/java/com/kishor/kotlin/designpatterns/BuilderPattern.kt
@@ -0,0 +1,21 @@
+package com.kishor.kotlin.designpatterns
+
+class OutFit(val shirt: Shirt, val pant: Pant, val shoes: Shoes) {
+
+    fun setShirt() {}
+    fun setPant() {}
+    // etc...
+}
+
+class Shoes {
+
+}
+
+class Pant {
+
+}
+
+
+class Shirt {
+
+}
