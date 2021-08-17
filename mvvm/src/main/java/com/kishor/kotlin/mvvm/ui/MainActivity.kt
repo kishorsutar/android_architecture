fbf488cb5b6b497c211a0fa8a159ad0892662b53
kishorsutar <kishor007sutar@gmail.com>
2021-08-17 14:30:00 +0100

    Added View Binding to load the fragment

--- mvvm/src/main/java/com/kishor/kotlin/mvvm/ui/MainActivity.kt
+++ mvvm/src/main/java/com/kishor/kotlin/mvvm/ui/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.kishor.kotlin.mvvm.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 import com.kishor.kotlin.mvvm.R
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<MusicListFragment>(R.id.fragment_container_view)
+            }
+        }
     }
+
 }
--- mvvm/src/main/java/com/kishor/kotlin/mvvm/ui/MusicListFragment.kt
+++ mvvm/src/main/java/com/kishor/kotlin/mvvm/ui/MusicListFragment.kt
@@ -8,10 +8,13 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.kishor.kotlin.mvvm.R
+import com.kishor.kotlin.mvvm.databinding.FragmentMusicListBinding
 import com.kishor.kotlin.mvvm.viewmodel.MusicListViewModel
-import kotlinx.android.synthetic.main.fragment_music_list.*
+import kotlinx.android.synthetic.main.fragment_music_list.view.*
 
 class MusicListFragment : Fragment(R.layout.fragment_music_list) {
+
+    private lateinit var _binding: FragmentMusicListBinding
 
     private val viewModel: MusicListViewModel by viewModels()
     override fun onCreateView(
@@ -19,9 +22,14 @@ class MusicListFragment : Fragment(R.layout.fragment_music_list) {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        Log.i("MusicListFragment","ViewModel created")
-        music_list.text = viewModel.displayString
-        return view
+        Log.i("MusicListFragment", "ViewModel created")
+        Log.d("View mode", viewModel.displayString)
+        _binding = FragmentMusicListBinding.inflate(inflater, container, false)
+        return _binding.root
     }
 
+    override fun onStart() {
+        super.onStart()
+        _binding.root.buttonPanel.text = "jerkweuahkueh"
+    }
 }
